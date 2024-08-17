b7ab5c5c7135b71fce4ff6d2f46bee257defec3f
Ezequiel Calore <ezequielcalore@hotmail.com>
2024-08-17 14:44:32 -0300

    Preentrega-1 JS

--- main.js
+++ main.js
@@ -2,10 +2,23 @@
 alert("¡Bienvenido al analista de funciones cuadráticas! \n A continuación, ingrese los coeficientes a,b y c de la función a analizar")
 
 // Coeficientes reales a,b y c de la función cuadrática a estudiar
-const coef_a = parseFloat(prompt("Ingrese el coeficiente a"))
-const coef_b = parseFloat(prompt("Ingrese el coeficiente b"))
-const coef_c = parseFloat(prompt("Ingrese el coeficiente c"))
+let coef_a = parseFloat(prompt("Ingrese el coeficiente a"))
+while (isNaN(coef_a) || coef_a==0) {
+    alert("El coeficiente a debe ser un número real no nulo")
+    coef_a = parseFloat(prompt("Ingrese el coeficiente a"))
+}
 
+let coef_b = parseFloat(prompt("Ingrese el coeficiente b"))
+while (isNaN(coef_b)) {
+    alert("El coeficiente b debe ser un número real")
+    coef_b = parseFloat(prompt("Ingrese el coeficiente b"))
+}
+
+const coef_c = parseFloat(prompt("Ingrese el coeficiente c"))
+while (isNaN(coef_c)) {
+    alert("El coeficiente c debe ser un número real")
+    coef_c = parseFloat(prompt("Ingrese el coeficiente c"))
+}
 
 // Función cuadrática construida
 function f(x) {
@@ -22,7 +35,7 @@ function discriminante(a,b,c) {
     return Math.pow(b,2)-4*a*c
 }
 
-// Cantidad de raíces de la función cuadrática
+// Cantidad de raíces reales de la función cuadrática
 function cant_raices(a,b,c) {
     if (discriminante(a,b,c)<0) {
         return 0
@@ -46,7 +59,7 @@ for (let i=-5; i<=5; i=i+1) {
 }
 
 // Interacción con el usuario
-let menu = parseInt(prompt("La función cuadrática introducida es: f(x)="+coef_a+"x^2+"+coef_b+"x+"+coef_c + "\n\n ¿Qué desea hallar? \n 1-Vértice \n 2-Raíces \n 3-Ordenada al origen \n 4-Eje de simetría \n 5-Imagen del conjunto A={-5,-4,...,4, 5} \n 6- Salir"))
+let menu = parseInt(prompt("La función cuadrática introducida es: f(x)=" + coef_a + "x^2+" + coef_b + "x+" + coef_c + "\n\n ¿Qué desea hallar? \n 1-Vértice \n 2-Raíces \n 3-Ordenada al origen \n 4-Eje de simetría \n 5-Imagen del conjunto A={-5,-4,...,4, 5} \n 6- Salir"))
 
 while (menu !== 6) {
     switch(menu) {
@@ -63,13 +76,13 @@ while (menu !== 6) {
             }
             break
         case 3:
-            alert("La ordenada al origen es y=" + coef_c)
+            alert("La ordenada al origen es: y=" + coef_c)
             break
         case 4:
             alert("El eje de simetría es: x=" + vertice(coef_a,coef_b,coef_c)[0])
             break
         case 5:
-            alert("El conjunto imagen de A es: {" + imagen_A + "}")
+            alert("El conjunto imagen de A es: f(A)={" + imagen_A + "}")
         default:
             break
     }
@@ -78,6 +91,4 @@ while (menu !== 6) {
 }
 
 alert("Gracias por su visita")
-
-
 
